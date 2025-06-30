15fe677625fa2d361e9e94f75bf28de2b188c28a
KawaiNime Pro <147604824+kawainime@users.noreply.github.com>
2025-06-30 20:19:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.glb', '**/*.gltf'], // Agar Vite bisa mengenali file model 3D
+})
